5fd802ed8df495c188bfefde15335ddcf57aa495
LukeAVanDrie <42616066+LukeAVanDrie@users.noreply.github.com>
2019-03-05 18:33:09 -0800

    Vispy test

---
+++ vispyTest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+from vispy import geometry, scene
+
+
+canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
+view = canvas.central_widget.add_view()
+
+#vertices, faces = geometry.create_sphere()
+sphere = scene.visuals.Sphere(radius=1, method='latitude', parent=view.scene,
+edge_color='black')
+
+camera = scene.cameras.TurntableCamera(fov=45, azimuth=-45, parent=view.scene)
+view.camera = camera
+
+if __name__ == '__main__' and sys.flags.interactive == 0:
+    canvas.app.run()
