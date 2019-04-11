58f04325ea70b2380e11cc05001d41a985129a67
LukeAVanDrie <42616066+LukeAVanDrie@users.noreply.github.com>
2019-04-10 21:16:42 -0500

    Added documentation

--- scripts/models/satellite.py
+++ scripts/models/satellite.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-# Models a satellite rotbiting Earth
+# Models a satellite orbiting Earth
 class Satellite:
-    in_view = False
-
     # Constructs a satellite
     def __init__(self, origin=np.array([0,0,0]), in_view = False):
+        # The location of the satellite in cartesian coordinates
         self.origin = origin
+
+        # Indicates whether the satellite is in view of a telescope
         self.in_view = in_view
--- scripts/models/telescope.py
+++ scripts/models/telescope.py
@@ -1,30 +1,40 @@
 import numpy as np
 
-
-# Models a telescope on the surface of the Earth.
+# Models a telescope
 class Telescope:
-    # Constructs a telescope.
+    # Constructs a telescope
     def __init__(self, name="", origin=np.array([0, 0, 0]), angle=0):
+        # The telescopes name
         self.name = name
+
+        # The location of the telescope in cartesian coordinates
         self.origin = origin
-        self.rad_angle = (angle * np.pi) / 180
+
+        # Half of the telescope's viewing angle in radians
+        rad_angle = (angle * np.pi) / 360
+
+        # The telescope's distance from the center of the Earth
         self.dist_origin = np.linalg.norm(origin)
+
+        # The slope of the telescope's viewing cone relative to a line passing through the 
+        # Earth's center and the telescope
         self.slope = np.tan((np.pi / 2) - self.rad_angle)
 
+    # Determines if the telescope can view a sattelite
     def can_view(self, point):
-        # Check if point is in correct direction
-        if (np.dot(point, self.origin) / np.linalg.norm(point) / np.linalg.norm(self.origin)) < 0:
+        # Determines if sattelite is in front of telescope
+        if (np.dot(point, self.origin) / np.linalg.norm(point) / self.dist_origin) < 0:
             return False
 
-        # Distance of point from center line
+        # Sattelite's distance from center line of the telescope's viewing cone
         intersection_point = np.cross(point, self.origin)
-        dist_x = np.linalg.norm(intersection_point / np.linalg.norm(self.origin))
+        dist_x = np.linalg.norm(intersection_point / self.origin)
 
-        # Distance along line from origin
+        # Sattelite's distance from the center of the Earth
         dist_point = np.linalg.norm(point)
         dist_y = np.sqrt(np.abs(np.power(dist_point, 2) - np.power(dist_x, 2)))
 
-        # Distance threshold for being withint the view of the telescope
+        # Distance threshold for being in view of the telescope
         threshold_y = self.dist_origin + self.slope * dist_x
 
         return threshold_y < dist_y
